abf90e8dcdcfa41da72758b31a4a63437e3a26e7
uzerhamzakhan-boop <uzerhamzakhan@gmail.com>
2026-03-11 14:22:56 -0400

    Update FileManager.java

--- FileManager.java
+++ FileManager.java
@@ -1,1 +1,23 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class FileManager {
+
+    public static void createFile(String name) {
+        try {
+            File file = new File(name);
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error occurred.");
+        }
+    }
+
+    public static void main(String[] args) {
+        createFile("test.txt");
+    }
+}
